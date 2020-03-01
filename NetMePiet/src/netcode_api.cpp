3f4a504504cb1b2b7255cbf7a9a78ab7177f8482
[JAM] Andreas <andreas.edmeier@web.de>
2020-03-01 18:56:41 +0100

    [PRO] BROKEN restructuring netcode

--- NetMePiet/src/netcode_api.cpp
+++ NetMePiet/src/netcode_api.cpp
@@ -7,144 +7,19 @@
 
 //===== ===== INTERN ===== =====
 #include "network/messages/message_base.hpp"
+#include "network/netcode_helper.hpp"
+#include "network/netcode_threads.hpp"
 
 #define STANDARD_URL "i.g-s.xyz"
 #define STANDARD_PORT 13370
 
 namespace NMP::Network {
 
-	moodycamel::ConcurrentQueue<Messages::Base*> incomingNetworkMessages {};
+	InQueue incomingNetworkMessages {};
+	OutQueue outMessages;
 
-	bool running = false;
+	volatile bool running = false;
 	std::thread listenThread;
-	TCPsocket tcpsock;
-
-	int Init() {
-		if(SDL_Init(0) == -1) {
-			printf("SDL_Init: %s\n", SDL_GetError());
-			return -1;
-		}
-		if(SDLNet_Init() == -1) {
-			printf("SDLNet_Init: %s\n", SDLNet_GetError());
-			return -2;
-		}
-
-		return 0;
-	}
-
-	Messages::Base* WaitForMessageComplete(uint8_t buffer[], size_t maxSize) {
-
-		//listen
-		auto result = SDLNet_TCP_Recv(tcpsock, buffer, 4);
-		if(result <= 0) {
-			// An error may have occured, but sometimes you can just ignore it
-			return nullptr;
-		}
-
-		uint32_t packageSize = ((uint32_t*)buffer)[0];
-		if(packageSize > maxSize) {
-			//TODO: incomming package bigger than buffer
-			return nullptr;
-		}
-
-		result = SDLNet_TCP_Recv(tcpsock, buffer, packageSize);
-		if(result <= 0) {
-			// An error may have occured, but sometimes you can just ignore it
-			return nullptr;
-		}
-
-		return Messages::Base::Deserialize(buffer, packageSize);
-	}
-
-	void ClientConnection(void) {
-		uint8_t buffer[1024];
-
-		while(running) {
-			Messages::Base* m = WaitForMessageComplete(buffer, 1024);
-
-			if(m != nullptr) {
-				incomingNetworkMessages.enqueue(m);
-			} else {
-				return;
-			}
-		}
-	}
-
-	void HandleConnection(TCPsocket socket, moodycamel::ConcurrentQueue<std::pair<TCPsocket, Messages::Base*>> *queue) {
-		uint8_t buffer[1024];
-
-		while(running) {
-			Messages::Base* m = WaitForMessageComplete(buffer, 1024);
-
-			if(m != nullptr) {
-				queue->enqueue(std::pair(socket, m));
-			} else {
-				return;
-			}
-		}
-	}
-
-	//clients have to send at least one package befor they can listen. so everyone who listens has to report himself to the network
-	void Relay(moodycamel::ConcurrentQueue<std::pair<TCPsocket, Messages::Base*>>* incomingClientNetworkMessages) {
-		std::set<TCPsocket> clients;
-
-		while(running) {
-			std::pair<TCPsocket, Messages::Base*> message;
-			while(incomingClientNetworkMessages->try_dequeue(message)) {
-				if(message.second == nullptr) {
-					continue;
-				}
-
-				for(auto it = clients.begin(); it != clients.end();) {
-					if(*it == message.first) {
-						continue;
-					}
-
-					if(!SendMessage(message.second, *it)) {
-						it = clients.erase(it);
-					} else {
-						it++;
-					}
-				}
-				delete(message.second);
-
-				if(message.first == nullptr) {
-					continue;
-				}
-
-				clients.insert(message.first);
-			}
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		}
-	}
-
-	void ServerAcceptNewConnections(void) {
-		std::vector<std::pair<TCPsocket, std::thread>> clientConnections;
-
-		// enqueued by another thread
-		moodycamel::ConcurrentQueue<std::pair<TCPsocket, Messages::Base*>> incomingClientNetworkMessages;
-
-		std::thread relay = std::thread(Relay, &incomingClientNetworkMessages);
-
-		while(running) {
-			TCPsocket new_tcpsock = SDLNet_TCP_Accept(tcpsock);
-			if(!new_tcpsock) {
-				//printf("SDLNet_TCP_Accept: %s\n", SDLNet_GetError());
-
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			} else {
-				printf("ne connection");
-				clientConnections.push_back(std::pair(new_tcpsock, std::thread(HandleConnection, new_tcpsock, &incomingClientNetworkMessages)));
-			}
-		}
-
-		relay.join();
-
-		for(size_t i = 0, size = clientConnections.size(); i < size; i++) {
-			clientConnections[i].second.join();
-		}
-	}
 
 	int InitServer(uint16_t port/* = 0000*/) {
 		int retVal = Init();
@@ -160,10 +35,8 @@ namespace NMP::Network {
 		ip.host = INADDR_ANY;
 		ip.port = port;
 
-		tcpsock = SDLNet_TCP_Open(&ip);
-
 		running = true;
-		listenThread = std::thread(ServerAcceptNewConnections);
+		listenThread = std::thread(ServerAcceptNewConnections, running, ip);
 
 		return 0;
 	}
@@ -189,14 +62,8 @@ namespace NMP::Network {
 			return 1;
 		}
 
-		tcpsock = SDLNet_TCP_Open(&ip);
-		if(!tcpsock) {
-			printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
-			return 2;
-		}
-
 		running = true;
-		listenThread = std::thread(ClientConnection);
+		listenThread = std::thread(ClientConnection, running, ip, incomingNetworkMessages, outMessages);
 
 		return 0;
 	}
@@ -212,27 +79,11 @@ namespace NMP::Network {
 		listenThread.join();
 	}
 
-	bool SendMessage(Messages::Base* message, TCPsocket socket/* = nullptr*/) {
+	void SendMessage(Messages::Base* message) {
 		if(message == nullptr) {
-			return true;
+			return;
 		}
 
-		if(socket == nullptr) {
-			socket = tcpsock;
-		}
-
-		uint8_t buffer[1024];
-		size_t packageSize = message->Serialize(buffer + 4, 1024);
-		((uint32_t*)buffer)[0] = (uint32_t)packageSize;
-
-		auto result = SDLNet_TCP_Send(socket, buffer, (int)packageSize + 4);
-		if(result < packageSize) {
-			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
-			return false;
-		}
-
-		incomingNetworkMessages.enqueue(message);
-
-		return true;
+		outMessages.enqueue(std::pair(nullptr, message));
 	}
 } // NMP::Network
---
+++ NetMePiet/src/netcode_helper.cpp
@@ -0,0 +1,75 @@
+#include "network/netcode_helper.hpp"
+
+//===== ===== EXTERN ===== =====
+#include <SDL.h>
+#include <SDL_net.h>
+#include <concurrentqueue.h>
+
+//===== ===== INTERN ===== =====
+
+namespace NMP::Network {
+
+	extern moodycamel::ConcurrentQueue<Messages::Base*> incomingNetworkMessages;
+
+	int Init() {
+		if(SDL_Init(0) == -1) {
+			printf("SDL_Init: %s\n", SDL_GetError());
+			return -1;
+		}
+		if(SDLNet_Init() == -1) {
+			printf("SDLNet_Init: %s\n", SDLNet_GetError());
+			return -2;
+		}
+
+		return 0;
+	}
+
+	Messages::Base* WaitForMessageComplete(TCPsocket listenerSocket, uint8_t buffer[], size_t maxSize) {
+
+		//listen
+		auto result = SDLNet_TCP_Recv(listenerSocket, buffer, 4);
+		if(result <= 0) {
+			// An error may have occured, but sometimes you can just ignore it
+			return nullptr;
+		}
+
+		uint32_t packageSize = ((uint32_t*)buffer)[0];
+		if(packageSize > maxSize) {
+			//TODO: incomming package bigger than buffer
+			return nullptr;
+		}
+
+		result = SDLNet_TCP_Recv(listenerSocket, buffer, packageSize);
+		if(result <= 0) {
+			// An error may have occured, but sometimes you can just ignore it
+			return nullptr;
+		}
+
+		return Messages::Base::Deserialize(buffer, packageSize);
+	}
+
+	bool DoSendMessage(Messages::Base* message, TCPsocket socket) {
+		if(message == nullptr) {
+			return true;
+		}
+
+		if(socket == nullptr) {
+			return false;
+		}
+
+		uint8_t buffer[1024];
+		size_t packageSize = message->Serialize(buffer + 4, 1024);
+		((uint32_t*)buffer)[0] = (uint32_t)packageSize;
+
+		auto result = SDLNet_TCP_Send(socket, buffer, (int)packageSize + 4);
+		if(result < packageSize) {
+			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+			return false;
+		}
+
+		incomingNetworkMessages.enqueue(message);
+
+		return true;
+	}
+
+} // NMP::Network
