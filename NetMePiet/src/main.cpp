013aee1c2c59493cdef9b8e253c6ef99caa5032e
Green Sky <green@g-s.xyz>
2020-03-04 17:29:21 +0100

    use imgui docking branch

--- NetMePiet/src/main.cpp
+++ NetMePiet/src/main.cpp
@@ -66,6 +66,8 @@ int main(int argc, char* argv[]) {
 		ImGuiIO& io = ImGui::GetIO();
 		(void)io;
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
 		ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
 		ImGui_ImplOpenGL3_Init();
